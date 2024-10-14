4ce8ab54d8d249ff45ee03e45a95053728635ac7
Naum Djordjevic <naumdjordjevic@Naums-MacBook-Pro.local>
2024-10-14 20:51:23 +0200

    wip - implementing firebase push notifications near end

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -8,6 +8,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     initKoin()
+    initPushNotifications()
 
     MaterialTheme {
         MainBottomNavigation()
--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -3,18 +3,33 @@ import FirebaseCore
 import FirebaseMessaging
 
 class AppDelegate: NSObject, UIApplicationDelegate {
+
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
       FirebaseApp.configure() //important
-      NotifierManager.shared.initialize(configuration: NotificationPlatformConfigurationIos.shared)
+
+      //By default showPushNotification value is true.
+      //When set showPushNotification to false foreground push notification will not be shown.
+      //You can still get notification content using #onPushNotification listener method.
+      NotifierManager.shared.initialize(configuration: NotificationPlatformConfigurationIos(
+            showPushNotification: true,
+            askNotificationPermissionOnStart: true)
+      )
 
     return true
   }
+
   func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         Messaging.messaging().apnsToken = deviceToken
   }
 
+  func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) async -> UIBackgroundFetchResult {
+        NotifierManager.shared.onApplicationDidReceiveRemoteNotification(userInfo: userInfo)
+        return UIBackgroundFetchResult.newData
+   }
 }
+
 @main
 struct iOSApp: App {
 
