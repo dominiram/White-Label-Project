c67c4365e157c9f1c0eca69b788b8239a783532c
Naum Djordjevic <naumdjordjevic@Naums-MacBook-Pro.local>
2024-12-17 01:29:45 +0100

    wip

--- composeApp/src/commonMain/kotlin/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/home/HomeScreen.kt
@@ -5,11 +5,9 @@ import androidx.compose.material3.DrawerState
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import com.multiplatform.webview.web.LoadingState
 import com.multiplatform.webview.web.WebView
 import com.multiplatform.webview.web.rememberWebViewState
 import models.NavigationItem
