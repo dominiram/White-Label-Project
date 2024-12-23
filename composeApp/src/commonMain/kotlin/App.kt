5ba674aaa26e4ed013a4ff0c7c51ced5368ec167
Naum Djordjevic <muckalica96@gmail.com>
2024-12-23 02:34:52 +0100

    working solution - implement it in a cleaner way

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -3,8 +3,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import config.Main
+import dev.icerock.moko.permissions.DeniedException
 import dev.icerock.moko.permissions.Permission
 import dev.icerock.moko.permissions.PermissionsController
+import dev.icerock.moko.permissions.RequestCanceledException
 import dev.icerock.moko.permissions.compose.BindEffect
 import dev.icerock.moko.permissions.compose.PermissionsControllerFactory
 import dev.icerock.moko.permissions.compose.rememberPermissionsControllerFactory
@@ -34,19 +36,34 @@ fun App() {
     MaterialTheme {
         viewModel.initPushNotificationToken()
         Main()
-        requestRuntimePermissions()
+        requestRuntimePermissions(Permission.REMOTE_NOTIFICATION)
     }
 }
 
 @Composable
-private fun requestRuntimePermissions() {
-    val factory: PermissionsControllerFactory = rememberPermissionsControllerFactory()
+private fun requestRuntimePermissions(permission: Permission) {
+    runCatching {
+        val factory: PermissionsControllerFactory = rememberPermissionsControllerFactory()
 
-    val controller: PermissionsController =
-        remember(factory) { factory.createPermissionsController() }
+        val controller: PermissionsController =
+            remember(factory) { factory.createPermissionsController() }
 
-    BindEffect(controller)
-    val coroutineScope: CoroutineScope = rememberCoroutineScope()
+        BindEffect(controller)
+        val coroutineScope: CoroutineScope = rememberCoroutineScope()
 
-    coroutineScope.launch { controller.providePermission(Permission.REMOTE_NOTIFICATION) }
+        coroutineScope.launch { grantPermissionIfNeeded(permission, controller) }
+    }
+}
+
+suspend fun grantPermissionIfNeeded(permission: Permission, controller: PermissionsController) {
+    if (!controller.isPermissionGranted(permission)) {
+        try {
+            controller.providePermission(permission)
+        } catch (e: DeniedException) {
+            controller.openAppSettings()
+        }
+        catch (e: RequestCanceledException) {
+            controller.openAppSettings()
+        } catch (e: Exception) { e.printStackTrace() }
+    }
 }
